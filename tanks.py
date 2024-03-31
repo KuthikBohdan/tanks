7ce47ce0f6a6b84de9ecbb74965dab9588f1cba9
KuthikBohdan <95546596+KuthikBohdan@users.noreply.github.com>
2024-03-31 14:46:26 +0300

    Update tanks.py

--- tanks.py
+++ tanks.py
@@ -127,7 +127,7 @@ class Block:
 
 bullets = []
 objects = []
-Tank('blue', 100, 275, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE), 15, 6, 10)
+Tank('blue', 100, 275, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE), 20, 5, 10)
 Tank('red', 650, 275, 0, (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_RCTRL), 5, 2, 20)
 for _ in range(10):
     while True:
